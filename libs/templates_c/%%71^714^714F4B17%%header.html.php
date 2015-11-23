73333a199c4e7250dd3449b0f5d58f15b21b7b13
Veselin <veselin_doynov@abv.bg>
2015-11-23 10:49:30 +0200

    Initial

---
+++ libs/templates_c/%%71^714^714F4B17%%header.html.php
@@ -0,0 +1,15 @@
+<?php /* Smarty version 2.6.19, created on 2015-11-12 15:56:17
+         compiled from header.html */ ?>
+<!DOCTYPE html>
+<head>
+<title><?php echo $this->_tpl_vars['title']; ?>
+</title>
+<meta charset="utf-8"> 
+<base href="<?php echo $this->_tpl_vars['PATH_TO_ROOT']; ?>
+/">
+<link rel="stylesheet" href="css/main.css" type="text/css"/>
+<div class="mainTitle"></div> 
+</head>
+<body>
+<div class="container">
+   
---
+++ libs/templates_c/%%80^806^80621A61%%footer.html.php
@@ -0,0 +1,5 @@
+<?php /* Smarty version 2.6.19, created on 2015-11-12 15:56:17
+         compiled from footer.html */ ?>
+</div>
+</body>
+</html>
